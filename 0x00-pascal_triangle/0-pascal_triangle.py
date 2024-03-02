7fb12a2444f53fff01438821059d2d9cb2eeb7b4
nwhator <nwhator@gmail.com>
2024-03-02 12:00:00 +0000

    Pascal Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,15 +11,16 @@ def pascal_triangle(n):
     Returns:
         list: A list of lists representing the Pascal's triangle.
     """
+
     # Check if n is less than or equal to 0
     if n <= 0:
         return []
 
     # Generate Pascal's Triangle using nested list comprehension
-    triangle = [[1] * (i + 1) for i in range(n)]
+    triangle = [[0] * (i + 1) for i in range(n)]
     
     # Populate the triangle except for the first row
-    for i in range(2, n):
+    for i in range(1, n):
         for j in range(1, i):
             triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
     
