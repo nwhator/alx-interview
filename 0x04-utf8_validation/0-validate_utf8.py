0811712e0037098139f32c77871ea13c75002b3a
nwhator <nwhator@gmail.com>
2024-04-04 12:00:00 +0000

    Implemented validUTF8 method for UTF-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,8 +14,12 @@ def validUTF8(data):
 
     for i in data:
         if byte_count == 0:
-            if i >> 5 == 0b110 or i >> 4 == 0b1110 or i >> 3 == 0b11110:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
                 byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
             elif i >> 7 == 0b1:
                 return False
         else:
